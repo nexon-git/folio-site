f6ad7053908d5dba88a03617c2adabc7d85fbcf7
nexon-git <profy3232@gmail.com>
2025-09-29 08:52:12 +0000

    nexon-folio-first

---
+++ src/components/About/index.js
@@ -0,0 +1,83 @@
+import { faCss3, faGitAlt, faHtml5, faJsSquare, faPython, faReact } from '@fortawesome/free-brands-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { useEffect, useState } from 'react'
+import Loader from 'react-loaders'
+import AnimatedLetters from '../AnimatedLetters'
+import './index.scss'
+
+
+const About = () => {
+
+
+
+
+    // this is a useState "hook". This "hook" sets the initial value for our classes.
+    const [letterClass, setLetterClass] = useState('text-animate')
+
+
+
+    // this is a hook used for changing classes after 4 seconds have passed.
+    useEffect(() => {
+        setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 3000)
+    }, [])
+
+
+
+    return (
+        <>
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    <AnimatedLetters
+                        letterClass={letterClass}
+                        strArray={['A', 'b', 'o', 'u', 't', ' ', 'm', 'e']}
+                        idx={15}
+                    />
+                </h1>
+                <p>
+                Hello my name is <em className='keywords'>Nexon</em>. I studied Computer Science in school and love researching AI. You can see my work on my portfolio page.
+                </p>
+                <p>
+                I am <em className='keywords'>looking to work remotely</em> as a member of a team to help create features for web and mobile apps. I am familiar with multiple coding languages such as <em className='keywords'>GitHub, Git, JavaScript, React, Node.js, Python, Next.js, TailwindCSS, Rapid API, and HTML.</em> 
+                </p>
+                </div>
+
+                <div className='stage-cube-cont'>
+                    <div className='cubespinner'>
+
+                        <div className='face1'>
+                            <FontAwesomeIcon icon={faPython} color="#4b8bbe" />
+                        </div>
+                        <div className='face2'>
+                            <FontAwesomeIcon icon={faHtml5} color="#F06529" />
+                        </div>
+                        <div className='face3'>
+                            <FontAwesomeIcon icon={faCss3} color="#28A4D9" />
+                        </div>
+                        <div className='face4'>
+                            <FontAwesomeIcon icon={faReact} color="#5ED4F4" />
+                        </div>
+                        <div className='face5'>
+                            <FontAwesomeIcon icon={faJsSquare} color="#EFD81D" />
+                        </div>
+                        <div className='face6'>
+                            <FontAwesomeIcon icon={faGitAlt} color="#EC4D28" />
+                        </div>
+
+                    </div>
+                </div>
+        </div>
+
+        {/* this Loader tag is used to import Loaders */}
+        <div className='Loader'>
+        <Loader type="pacman" />
+        </div>
+        </>
+    )
+}
+
+
+
+export default About
---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,154 @@
+// first thing to do is important styles
+
+import './index.scss'
+import { useState } from 'react'
+import LogoS from '../../assets/images/logo-s.png'
+// imports LogoS from the assets folder
+// import LogoSubtitle from '../../assets/images/logo_sub.png'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {
+  faEnvelope,
+  faHome,
+  faSuitcase,
+  faUser,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons' // add faTwitter and faSkype to the import
+import { Link, NavLink } from 'react-router-dom'
+
+const Sidebar = () => {
+  const [showNav, setShowNav] = useState(false)
+
+  return (
+    <div className="nav-bar">
+      {/* this is edited out because I don't have my own logo yet. */}
+
+      <Link className="logo" to="/">
+        <img src={LogoS} alt="Logo" />
+
+        {/* <img className="sub-logo" src={LogoSubtitle} alt="Name: Diego Espino" /> */}
+      </Link>
+
+      <nav className={showNav ? 'mobile-show' : ''}>
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          to="/"
+        >
+          <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="about-link"
+          to="/about"
+        >
+          <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+        </NavLink>
+
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="portfolio-link"
+          to="/portfolio"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink>
+
+        {/* <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="blog-link"
+          to="/blog"
+        >
+          <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+        </NavLink> */}
+
+        <NavLink
+          onClick={() => setShowNav(false)}
+          exact="true"
+          activeclassname="active"
+          className="contact-link"
+          to="/contact"
+        >
+          <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+        </NavLink>
+
+        <FontAwesomeIcon
+          onClick={() => setShowNav(false)}
+          icon={faClose}
+          color="#ffd700"
+          size="3x"
+          className="close-icon"
+        />
+      </nav>
+
+      <ul>
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.linkedin.com/in/developerdiego/"
+          >
+            <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+          </a>
+        </li>
+
+        <li>
+          <a
+            target="_blank"
+            rel="noreferrer"
+            href="https://github.com/DailyDisco"
+          >
+            <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+          </a>
+        </li>
+
+        {/* <li>
+                <a
+
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://twitter.com/digitaIDiego'
+                
+                >
+                    <FontAwesomeIcon icon={faTwitter} color="#4d4d4e" />
+                </a>
+            </li> */}
+
+        {/* this is edited out because I don't use these socials yet.
+
+            <li>
+                <a
+
+                target="_blank" 
+                rel='noreferrer' 
+                href='https://github.com/DailyDisco'
+                
+                >
+                    <FontAwesomeIcon icon={faSkype} color="#4d4d4e" />
+                </a>
+            </li>
+
+    */}
+      </ul>
+
+      {/* hamburger menu */}
+      <FontAwesomeIcon
+        onClick={() => setShowNav(true)}
+        icon={faBars}
+        color="#ffd700"
+        size="3x"
+        className="hamburger-icon"
+      />
+    </div>
+  )
+}
+
+export default Sidebar
